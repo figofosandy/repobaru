7c9252aeb196b27258450916f076039444162d5e
figofosandy <figofosandy@gmail.com>
2019-10-02 07:33:35 +0000

    update

--- week1/day5/KonversiSuhu.java
+++ week1/day5/KonversiSuhu.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class KonversiSuhu {
     private String suhuAwal, suhuAkhir;
     private double nilaiAwal, nilaiAkhir;
+    private String[] celcius = new String[] {"c","ce","cel","celc","celci","celciu","celcius"};
+    private String[] reamur = new String[] {"r","re","rea","ream","reamu","reamur"};
+    private String[] farenheit = new String[] {"f","fa","far","fare","faren","farenh","farenhe","farenhei","farenheit"};
+    private boolean celciusBoolean=Arrays.asList(celcius).stream().anyMatch(x->x.equalsIgnoreCase(suhuAwal));
+    private boolean reamurBoolean=Arrays.asList(reamur).stream().anyMatch(x->x.equalsIgnoreCase(suhuAwal));
+    private boolean farenheitBoolean=Arrays.asList(farenheit).stream().anyMatch(x->x.equalsIgnoreCase(suhuAwal));
     public KonversiSuhu(String suhuAwal, String suhuAkhir, double nilaiAwal) {
         this.suhuAwal=suhuAwal;
         this.suhuAkhir=suhuAkhir;
@@ -12,43 +19,43 @@ public class KonversiSuhu {
         output();
     }
     public double konversi() {
-        if(suhuAwal.equalsIgnoreCase("celcius")) {
-            if(suhuAkhir.equalsIgnoreCase("celcius")) {
+        if(celciusBoolean) {
+            if(celciusBoolean) {
                 return nilaiAkhir=nilaiAwal;
             }
-            else if(suhuAkhir.equalsIgnoreCase("reamur")) {
+            else if(reamurBoolean) {
                 return nilaiAkhir=nilaiAwal*4/5;
             }
-            else if(suhuAkhir.equalsIgnoreCase("farenheit")) {
-                return nilaiAkhir=(nilaiAwal*9/5)+32;    
+            else if(farenheitBoolean) {
+                return nilaiAkhir=(nilaiAwal*9/5)+32;
             }
             else {
                 return nilaiAkhir=0;
             }
         }
-        else if(suhuAwal.equalsIgnoreCase("reamur")) {
-            if(suhuAkhir.equalsIgnoreCase("celcius")) {
-                return nilaiAkhir=nilaiAwal*5/4;    
+        else if(reamurBoolean) {
+            if(celciusBoolean) {
+                return nilaiAkhir=nilaiAwal*5/4;
             }
-            else if(suhuAkhir.equalsIgnoreCase("reamur")) {
+            else if(reamurBoolean) {
                 return nilaiAkhir=nilaiAwal;
             }
-            else if(suhuAkhir.equalsIgnoreCase("farenheit")) {
-                return nilaiAkhir=(nilaiAwal*9/4)+32;    
+            else if(farenheitBoolean) {
+                return nilaiAkhir=(nilaiAwal*9/4)+32;
             }
             else {
                 return nilaiAkhir=0;
             }
         }
-        else if(suhuAwal.equalsIgnoreCase("farenheit")) {
-            if(suhuAkhir.equalsIgnoreCase("celcius")) {
-                return nilaiAkhir=(nilaiAwal-32)*5/9;    
+        else if(farenheitBoolean) {
+            if(celciusBoolean) {
+                return nilaiAkhir=(nilaiAwal-32)*5/9;
             }
-            else if(suhuAkhir.equalsIgnoreCase("reamur")) {
+            else if(reamurBoolean) {
                 return nilaiAkhir=(nilaiAwal-32)*4/9;
             }
-            else if(suhuAkhir.equalsIgnoreCase("farenheit")) {
-                return nilaiAkhir=nilaiAwal;    
+            else if(farenheitBoolean) {
+                return nilaiAkhir=nilaiAwal;
             }
             else {
                 return nilaiAkhir=0;
@@ -71,6 +78,6 @@ public class KonversiSuhu {
         System.out.print("Masukkan Nilai awal : ");
         Double nlAwal=sc.nextDouble();
         new KonversiSuhu(shAwal, shAkhir, nlAwal);
-        sc.close();   
+        sc.close();
     }
 }

2d3f3716a677fe4c0d4b24f7d20f1d3b3b024873
figofosandy <figofosandy@gmail.com>
2019-10-23 17:23:55 +0700

    Week 5 Day 3 : Learn Hapi JS

---
+++ week5/myproject/index.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const Hapi=require('@hapi/hapi');
+
+const Joi=require('@hapi/joi');
+
+const server = new Hapi.server({
+    port:3101,
+    host:'localhost'
+});
+
+server.route([
+    {
+        method:'GET',
+        path:'/',
+        handler:(request,h)=>{
+            return 'I am root route';
+        }
+    },
+    {
+        method:'GET',
+        path:'/hello',
+        handler:(request,h)=>{
+            return{msg:'こんにちは'};
+        }
+    },
+    {
+        method:'POST',
+        path:'/hellopost',
+        handler:(request,h)=>{
+            return{msg:'using method post'};
+        }
+    },
+    {
+        method:'POST',
+        path:'/persegi',
+        config:{
+            validate:{
+                payload:{
+                    panjang:Joi.number().min(1).required(),
+                    lebar:Joi.number().min(1).required()
+                }
+            }
+        },
+        handler:(request,h)=>{
+            console.log(request.payload);
+                let panjangRequest=parseInt(request.payload.panjang);
+                let lebarRequest=parseInt(request.payload.lebar);
+                let hasil=panjangRequest*lebarRequest;
+                    const data={data:'hello detail users',...request.payload,hasil:hasil}
+                    return h.response(data).code(200)
+        }
+    },
+    {
+        method:'POST',
+        path:'/ganjilgenap',
+        config:{
+            validate:{
+                payload:{
+                    angka:Joi.number().required()
+                }
+            }
+        },
+        handler:(request,h)=>{
+            console.log(request.payload);
+                let angka=parseInt(request.payload.angka);
+                let hasil;
+                let template=' adalah bilangan ';
+                if(angka<0) {
+                    hasil=angka+template+'negatif';
+                } else if(angka==0) {
+                    hasil=angka+template+angka;
+                } else if(angka%2==0) {
+                    hasil=angka+template+'genap';
+                } else if(angka%2==1) {
+                    hasil=angka+template+'ganjil';
+                }              
+                    const data={...request.payload,hasil:hasil}
+                    return h.response(data).code(200)
+        }
+    }
+]);
+
+const main=async()=>{
+    await server.register(require('./src/routes/users'));
+    await server.start();
+    return server;
+}
+
+main().then(server=>{
+    console.log('Server running at:',server.info.uri);
+}).catch(err=>{
+    console.log(err);
+    process.exit(1);
+})
